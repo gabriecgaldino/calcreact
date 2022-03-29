25815eea930950e8fec7b8d31c64f150607f24d4
gabrielcgaldino <gabrielcgaldiino@gmail.com>
2022-03-29 11:05:44 -0300

    Nova correção de files/folders para gitPages

--- webpack.config.js
+++ webpack.config.js
@@ -3,13 +3,13 @@ module.exports = {
     mode: 'development',
     entry: path.resolve(__dirname, 'src', 'index.js'),
     output: {
-        path: path.resolve(__dirname, 'docs'),
+        path: path.resolve(__dirname),
         filename: 'bundle.js'
     },
     // configuração do webpackdevserver
     devServer: {
         static: {
-          directory: path.join(__dirname, 'docs'),
+          directory: path.join(__dirname),
         },
         compress: true,
         port: 9000,
